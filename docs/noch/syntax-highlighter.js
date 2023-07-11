5c23113b1859a9fd6ba909b482b1aa8ae4f6c787
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2023-07-11 16:44:13 -0400

    added keyword and directive list

---
+++ docs/noch/syntax-highlighter.js
@@ -0,0 +1,16 @@
+const cDirectives = [
+    "include", "define", "else", "elif",
+    "endif", "error", "if", "ifdef",
+    "ifndef", "pragma", "undef"
+];
+
+const cKeywords = [
+    "auto",	"break", "case", "char",
+    "const", "continue", "default",	"do",
+    "double", "else", "enum", "extern",
+    "float", "for",	"goto",	"if",
+    "int", "long", "register", "return",
+    "short", "signed", "sizeof", "static",
+    "struct", "switch",	"typedef", "union",
+    "unsigned",	"void",	"volatile", "while"
+];
